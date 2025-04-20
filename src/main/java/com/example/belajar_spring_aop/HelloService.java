790493d2b94f849bd2fd6aad27ec32a5fcb4d3cc
puspakirana <puspakiranastv@gmail.com>
2025-04-20 18:24:32 +0700

    first commit

---
+++ src/main/java/com/example/belajar_spring_aop/HelloService.java
@@ -0,0 +1,10 @@
+package com.example.belajar_spring_aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloService {
+    
+}
