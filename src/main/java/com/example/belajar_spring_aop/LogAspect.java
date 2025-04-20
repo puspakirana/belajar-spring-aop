ea59b01e0762ed308310c9eba943903afa88e7fe
puspakirana <puspakiranastv@gmail.com>
2025-04-20 18:41:14 +0700

    Pointcut

--- src/main/java/com/example/belajar_spring_aop/LogAspect.java
+++ src/main/java/com/example/belajar_spring_aop/LogAspect.java
@@ -1,10 +1,16 @@
 package com.example.belajar_spring_aop;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
+
+    @Pointcut("")
+    public void helloServiceMethod() {
+
+    }
     
 }
