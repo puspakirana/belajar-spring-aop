fc91e032a5b06e43d9136b565a6a093aa48af2b3
puspakirana <puspakiranastv@gmail.com>
2025-04-20 19:23:26 +0700

    Proceeding Join Point

--- src/main/java/com/example/belajar_spring_aop/aspect/LogAspect.java
+++ src/main/java/com/example/belajar_spring_aop/aspect/LogAspect.java
@@ -1,6 +1,8 @@
 package com.example.belajar_spring_aop.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -28,9 +30,20 @@ public class LogAspect {
         log.info("Before " + className + "." + methodName + "()");
     }
 
-    @Before("helloServiceMethod()")
-    public void beforeHelloServiceMethodAgain() {
-        log.info("Before HelloService Method Again");
+    @Around("helloServiceMethod()")
+    public Object aroundHelloServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Before " + className + "." + methodName + "()");
+        try {
+            log.info("Around before " + className + "." + methodName + "()");
+            return joinPoint.proceed(joinPoint.getArgs());
+        } catch (Throwable throwable) {
+            log.info("Around throwable " + className + "." + methodName + "()");
+            throw throwable;
+        } finally {
+            log.info("Around finally " + className + "." + methodName + "()");
+        }
     }
     
 }
