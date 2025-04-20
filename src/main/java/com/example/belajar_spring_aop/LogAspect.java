83629de8199b8d32fc788cacb6de9c263bf0e30b
puspakirana <puspakiranastv@gmail.com>
2025-04-20 18:34:43 +0700

    Aspect

---
+++ src/main/java/com/example/belajar_spring_aop/LogAspect.java
@@ -0,0 +1,10 @@
+package com.example.belajar_spring_aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+    
+}
